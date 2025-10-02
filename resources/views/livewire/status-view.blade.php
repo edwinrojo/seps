e5200e1d90d66eab6bcb68193c1ff71541cec7f9
edwinrojo <edwinrojo15@gmail.com>
2025-10-02 15:48:19 +0800

    status view and office settings

--- resources/views/livewire/status-view.blade.php
+++ resources/views/livewire/status-view.blade.php
@@ -10,13 +10,15 @@
             $filament_icon_color = 'text-' . $status->status->getColor() . '-500';
 
             $avatar = $status->user->getFilamentAvatarUrl();
+            $status_color = $status->status->getColor();
+            $status_label = $status->status->getLabel();
         @endphp
         <li class="mb-6 ms-6">
-            <span class="absolute flex items-center justify-center w-6 h-6 {{ $icon_background }} rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 {{ $icon_background_dark }}">
-                  <x-dynamic-component :component="$filament_icon" class="w-4 h-4 text-custom-800 fi-color-{{ $status->status->getColor() }}"/>
+            <span class="absolute flex items-center justify-center w-7 h-7 {{ $icon_background }} rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 {{ $icon_background_dark }}">
+                  <x-dynamic-component :component="$filament_icon" class="w-4 h-4 text-custom-800 fi-color-{{ $status_color }}"/>
             </span>
 
-            <h3 class="flex items-center mb-1 text-base font-semibold text-custom-800 fi-color-{{ $status->status->getColor() }} dark:text-white">{{ $status->status->getLabel() }}
+            <h3 class="flex items-center mb-1 text-base font-semibold text-custom-800 fi-color-{{ $status_color }} dark:text-white">{{ $status_label }}
                 {{-- display if index is 0 --}}
                 @if ($loop->index === 0)
                     <span class="bg-primary-100 text-primary-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-primary-900 dark:text-primary-300 ms-3">Latest</span>
@@ -25,7 +27,7 @@
 
 
 
-            <div class="flex items-start gap-2.5">
+            <div class="flex items-start gap-2.5 mt-3">
                 <img class="w-8 h-8 rounded-full" src="{{ $avatar }}" alt="Bonnie Green image">
                 <div class="flex flex-col gap-1">
                     <div class="flex flex-col w-full leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
