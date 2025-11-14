ab76476efd13c6ecaa9624dbe310aaf23c59a246
edwinrojo <edwinrojo15@gmail.com>
2025-11-14 23:08:44 +0800

    enable password reset

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login(Login::class)
+            ->passwordReset()
             ->darkMode(false)
             ->globalSearch(false)
             ->databaseNotifications()
