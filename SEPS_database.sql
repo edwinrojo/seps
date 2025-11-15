34c10943556849dccd40c9b2917d1bd60b69317e
edwinrojo <edwinrojo15@gmail.com>
2025-11-15 15:05:10 +0800

    include database creation

--- SEPS_database.sql
+++ SEPS_database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `seps` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `seps`;
 -- MySQL dump 10.13  Distrib 8.0.44, for macos15 (arm64)
 --
 -- Host: localhost    Database: seps
@@ -741,4 +743,4 @@ CREATE TABLE `validation_purposes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-15 15:01:06
+-- Dump completed on 2025-11-15 15:04:38
